25c4367f4c7ef50a33e5a55d44935d2f07130edc
jfonbe <jan.bergmannfonseca@gmail.com>
2026-07-20 14:45:45 +0200

    add: more grid-values

--- src/data/settingsOptions.ts
+++ src/data/settingsOptions.ts
@@ -3,17 +3,30 @@ import type { SettingsOptions } from "../types/components"
 export const settingsOptions: SettingsOptions = {
     grid: {
         title: "Grid",
-        options: [{
-            label: "5x5",
-            value: {rows: 5, cols: 5}
+        options: [
+        {
+            label: "3x3",
+            value: {rows: 3, cols: 3}
         },
         {
             label: "4x4",
             value: {rows: 4, cols: 4}
         },
         {
+            label: "5x5",
+            value: {rows: 5, cols: 5}
+        },
+        {
             label: "6x6",
             value: {rows: 6, cols: 6}
+        },
+        {
+            label: "7x7",
+            value: {rows: 7, cols: 7}
+        },
+        {
+            label: "8x8",
+            value: {rows: 8, cols: 8}
         },
         ]
     },
