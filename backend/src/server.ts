d11e4885cf1457140e4d15651ef35de44162f792
jfonbe <jan.bergmannfonseca@googlemail.com>
2026-08-07 20:11:27 +0200

    add: backend data

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+import data from "./data/data.js"
+
+const app = express()
+const PORT = 1234
+
+app.get("/", (req, res) => {
+    res.send(data)
+})
+
+app.listen(PORT, () => {
+    console.log("Server läuft")
+})
