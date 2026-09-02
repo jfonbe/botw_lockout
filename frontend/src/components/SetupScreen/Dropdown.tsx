bf09c3da6346d0302544ae649ca436cbcbebc6e3
jfonbe <jan.bergmannfonseca@gmail.com>
2026-09-02 16:20:57 +0200

    fix: dropdown visibility

--- frontend/src/components/SetupScreen/Dropdown.tsx
+++ frontend/src/components/SetupScreen/Dropdown.tsx
@@ -66,25 +66,20 @@ export default function Dropdown<T> ({inputOptions, label, onChange, variant, ch
             >
                 {label}
             </button>
-
-            {isOpen && (
-                <div
-                    className={`${styles.dropdownMenu} ${isOpen ? styles.visible : styles.hidden}`}
-                >
-                    {inputOptions.options.map((option => (
-                        <DropdownButton
-                            value={option.value}
-                            label={option.label}
-                            onChange={handleSelect}
-                            variant={variant}
-                            key={option.label}
-                        />
-                    )))}
-
-                    {children?.(closeDropdown)}
-
-                </div>
-            )}
+            <div
+                className={`${styles.dropdownMenu} ${isOpen ? styles.visible : styles.hidden}`}
+            >
+                {inputOptions.options.map((option => (
+                    <DropdownButton
+                        value={option.value}
+                        label={option.label}
+                        onChange={handleSelect}
+                        variant={variant}
+                        key={option.label}
+                    />
+                )))}
+                {children?.(closeDropdown)}
+            </div>
         </div>
     )
 }
